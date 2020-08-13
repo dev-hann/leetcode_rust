c36764d66e224c1b7c0103eadf8ee1e701e92268
yoehwan <yoehwan@gmail.com>
2020-08-13 09:16:22 +0800

    1480

--- _1480/src/main.rs
+++ _1480/src/main.rs
@@ -1,3 +1,17 @@
+struct Solution {}
+
 fn main() {
-    println!("Hello, world!");
+    let sol = Solution::running_sum(vec![1, 2, 3,4]);
+    println!("{:?}", sol)
+}
+
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut res = Vec::new();
+        res.push(nums[0]);
+        for i in 1..nums.len() {
+            res.push(nums[i]+res[i-1]);
+        }
+        res
+    }
 }
---
+++ _1480/target/debug/deps/_1480-afc684c317104e81.d
@@ -0,0 +1,7 @@
+D:\rust_project\leetcode\_1480\target\debug\deps\_1480-afc684c317104e81.rmeta: src\lib.rs
+
+D:\rust_project\leetcode\_1480\target\debug\deps\lib_1480-afc684c317104e81.rlib: src\lib.rs
+
+D:\rust_project\leetcode\_1480\target\debug\deps\_1480-afc684c317104e81.d: src\lib.rs
+
+src\lib.rs:
