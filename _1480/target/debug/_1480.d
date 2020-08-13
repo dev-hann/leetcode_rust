40dac795131b0b266d7abf0aaf97ec46a126ac28
dev-hann <developer.hann@gmail.com>
2020-08-13 09:16:22 +0800

    1480

---
+++ _1480/target/debug/_1480.d
@@ -0,0 +1,1 @@
+D:\rust_project\leetcode\_1480\target\debug\_1480.exe: D:\rust_project\leetcode\_1480\src\main.rs
---
+++ _1480/target/debug/deps/_1480.d
@@ -0,0 +1,5 @@
+D:\rust_project\leetcode\_1480\target\debug\deps\_1480.exe: src\main.rs
+
+D:\rust_project\leetcode\_1480\target\debug\deps\_1480.d: src\main.rs
+
+src\main.rs:
