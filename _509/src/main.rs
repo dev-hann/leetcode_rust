2e6f17d8980e2dd1a80a049a9b18d12fb99d14f5
dev-hann <developer.hann@gmail.com>
2021-01-06 13:10:14 +0800

    _70

--- _509/src/main.rs
+++ _509/src/main.rs
@@ -5,8 +5,9 @@ fn main() {
 
 
 struct Solution {}
-
-impl Solution {
+//Runtime: 0 ms, faster than 100.00% of Rust online submissions for Fibonacci Number.
+// Memory Usage: 2.3 MB, less than 7.89% of Rust online submissions for Fibonacci Number. ->can reduce?
+/*impl Solution {
     pub fn fib(n: i32) -> i32 {
         if n==0 || n==1{
             return n
@@ -17,5 +18,16 @@ impl Solution {
             fib_list.remove(0);
         }
         return fib_list[fib_list.len()-1];
+    }
+}*/
+
+//Runtime: 8 ms, faster than 12.50% of Rust online submissions for Fibonacci Number.
+// Memory Usage: 2.1 MB, less than 45.83% of Rust online submissions for Fibonacci Number.
+impl Solution {
+    pub fn fib(n: i32) -> i32 {
+        if n<=1{
+            return n
+        }
+      return Self::fib(n-1)+Self::fib(n-2)
     }
 }
