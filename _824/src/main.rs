01cc48b5a69e59e69bab663f1348ad19cc1e3c53
han <yoehwan@gmail.com>
2021-01-14 13:33:45 +0800

    not yet.. need study String str ..

--- _824/src/main.rs
+++ _824/src/main.rs
@@ -1,29 +1,44 @@
+use std::borrow::Borrow;
+
 fn main() {
     let res = Solution::to_goat_latin("I speak Goat Latin".to_string());
     println!("{}", res)
 }
 
 struct Solution {}
-Not yet solved!
+
+//Not yet solved!
 impl Solution {
-    fn isContainVowel(str: &str) -> bool {
-        let _vowel: Vec<&str> = vec!["a", "e", "i", "o", "u"];
+    fn is_begin_vowel(str: &str) -> bool {
+        let _vowel: Vec<char> = vec!['a', 'e', 'i', 'o', 'u'];
+        let a = str.chars().collect::<Vec<char>>()[0].to_ascii_lowercase();
         for v in _vowel.iter() {
-            if (str.to_lowercase()).contains(v) {
-                return true
+            if a == *v {
+                return true;
             }
         }
         return false;
     }
 
     pub fn to_goat_latin(s: String) -> String {
-        let mut res: Vec<&str> = s.split(" ").collect();
-        for mut s in res.iter() {
-            if Self::isContainVowel(s) {
-                s= &&*(s.clone().to_owned() + "ma");
+        let res: Vec<&str> = s.split(" ").collect();
+        let mut _r: String = "".to_string();
+        for s in res.iter() {
+            if Self::is_begin_vowel(s) {
+            //    print!("{:?}", s);
+                let k = s.to_string()+" ";
+                _r.push_str(k.borrow());
+                continue;
             }
-        }
+            let mut a: Vec<char> = s.chars().collect();
+            a.push(a[0]);
+            a.remove(0);
 
+            //print!("{:?}", (a.iter().collect::<String>()));
+           _r.push_str(&a.iter().collect::<String>());
+
+        }
+        println!("{}", _r);
 
         return res.join(" ");
     }
