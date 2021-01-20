c059540355202dbf93a4930da61c535b014aa26c
dev-hann <developer.hann@gmail.com>
2021-01-20 13:20:18 +0800

    _389

--- _389/src/main.rs
+++ _389/src/main.rs
@@ -1,26 +1,50 @@
-use std::collections::HashMap;
+
 
 fn main() {
-        let res = Solution::find_the_difference(String::from("abcd"),String::from("abcde"));
-    println!("{}",res)
+    let res = Solution::find_the_difference(String::from("a"), String::from("aa"));
+    println!("{}", res)
 }
 
 struct Solution;
-Not Yet Solve
+// Runtime: 0 ms, faster than 100.00% of Rust online submissions for Find the Difference.
+// Memory Usage: 2.1 MB, less than 69.57% of Rust online submissions for Find the Difference.
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut char_table:HashMap<char,i32> = HashMap::new();
-        for c in t.chars(){
-            char_table.insert(c,);
-        }
-        for c in s.chars(){
-            char_table.insert(c,false);
-        }
-        for (key,value) in char_table{
-            if value{
-                return key
-            }
-        }
-        return 'c';
+        let t_sum: u8 = t.chars().map(|x|x as u8).sum();
+        let s_sum: u8 = s.chars().map(|x|x as u8).sum();
+        return char::from(t_sum-s_sum);
     }
 }
+
+
+// Runtime: 0 ms, faster than 100.00% of Rust online submissions for Find the Difference.
+// Memory Usage: 2.2 MB, less than 8.70% of Rust online submissions for Find the Difference.
+// toooo much memory
+/*
+impl Solution {
+    pub fn find_the_difference(s: String, t: String) -> char {
+        let mut char_table: HashMap<char, i32> = HashMap::new();
+        for c in t.chars() {
+            let v = match char_table.get(&c) {
+                Some(char) => char,
+                None => &0
+            };
+            char_table.insert(c, *v + 1);
+        }
+        for c in s.chars() {
+            let v = match char_table.get(&c) {
+                Some(char) => char,
+                None => &0
+            };
+            char_table.insert(c, *v - 1);
+        }
+        let mut res = ' ';
+        for (c, v) in char_table.iter() {
+            if *v == 1 {
+                res = *c;
+            }
+        }
+
+        return res;
+    }
+}*/
