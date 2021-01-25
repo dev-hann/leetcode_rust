30a63a16da048e56f0f142391cad0a9dbf5ad4a0
dev-hann <developer.hann@gmail.com>
2021-01-25 13:34:42 +0800

    optimizing..

--- _459/src/main.rs
+++ _459/src/main.rs
@@ -1,24 +1,47 @@
-
 fn main() {
-
-    let res: bool = Solution::repeated_substring_pattern("abab".to_string());
+    let res: bool = Solution::repeated_substring_pattern("aba".to_string());
 
     println!("{}", res)
 }
 
 struct Solution {}
 
-
+Not Yet!!
 impl Solution {
     pub fn repeated_substring_pattern(s: String) -> bool {
-       let len = s.len();
-        if len<= 1{
-            return false;
+        let len = s.len();
+
+        let mut one = "".to_string();
+        for i in 0..len/2 {
+
+            let (one,other) = s.split_at(i);
+           if other.replace(one,"")==""{
+               return true
+           }
         }
-        let chars:Vec<char> = s.chars().collect();
-        let mut p =1;
 
-
-        return false
+        return false;
     }
 }
+
+// Runtime: 12 ms, faster than 83.33% of Rust online submissions for Repeated Substring Pattern.
+// Memory Usage: 2.1 MB, less than 50.00% of Rust online submissions for Repeated Substring Pattern.
+/*impl Solution {
+    pub fn repeated_substring_pattern(s: String) -> bool {
+
+        let len = s.len();
+
+        let mut split_cnt=2;
+        while split_cnt<=len {
+            if len%split_cnt==0{
+                let (one,other)= s.split_at(len/split_cnt);
+                let res = other.replace(one,"");
+                if res==""{
+                    return  true
+                }
+            }
+            split_cnt+=1;
+        }
+        return false
+    }
+}*/
--- _459/target/debug/_459.d
+++ _459/target/debug/_459.d
@@ -1,1 +1,1 @@
-/home/yoehwan/RustProject/leetcode_rust/_459/target/debug/_459: /home/yoehwan/RustProject/leetcode_rust/_459/src/main.rs
+D:\leetcode_rust\_459\target\debug\_459.exe: D:\leetcode_rust\_459\src\main.rs
