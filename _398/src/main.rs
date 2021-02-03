9d360cf2ea02874c50a2f47f09cc1fdbc6cafd8e
han <yoehwan@gmail.com>
2021-02-03 13:22:10 +0800

    _398

--- _398/src/main.rs
+++ _398/src/main.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-use rand::seq::SliceRandom;
+use rand::prelude::*;
 
 fn main() {
     let nums = vec![1, 2, 3, 3, 3];
@@ -21,30 +20,28 @@ fn main() {
  * let ret_1: i32 = obj.pick(target);
  */
 
-not yet!!
-
-// Runtime: 64 ms, faster than 14.29% of Rust online submissions for Random Pick Index.
-// Memory Usage: 5.7 MB, less than 14.29% of Rust online submissions for Random Pick Index.
+// Runtime: 16 ms, faster than 100.00% of Rust online submissions for Random Pick Index.
+// Memory Usage: 4.5 MB, less than 71.43% of Rust online submissions for Random Pick Index.
 struct Solution {
-    map: HashMap<i32,Vec<usize>>,
+  nums:Vec<i32>
 }
 
 impl Solution {
     fn new(nums: Vec<i32>) -> Self {
-        let mut _tmp_hash: HashMap<i32,Vec<usize>> = HashMap::new();
-        for (index, value) in nums.iter().enumerate() {
-            match _tmp_hash.get_mut(value) {
-                None => { _tmp_hash.insert(*value, vec![index]); }
-                Some(va) => {va.push(index)}
-            }
-        }
-        return Solution { map: _tmp_hash };
+        return Solution { nums };
     }
 
     fn pick(&self, target: i32) -> i32 {
-        let mut rng = rand::thread_rng();
-        let res = self.map.get(&target).unwrap();
-        let x = res.choose(&mut rng).unwrap();
-        return *x as i32
+        let mut res:Vec<usize>=Vec::new();
+
+        for (index,value) in self.nums.iter().enumerate(){
+            if *value!=target{
+                continue;
+            }
+            res.push(index)
+        }
+
+        let mut rng=rand::thread_rng();
+        return *(res.choose(&mut rng).unwrap()) as i32;
     }
 }
