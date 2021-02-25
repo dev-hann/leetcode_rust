618f8abaf84dd729d77635feb06750879b40f338
han <yoehwan@gmail.com>
2021-02-25 09:18:00 +0800

    test

--- _868/src/main.rs
+++ _868/src/main.rs
@@ -5,29 +5,6 @@ fn main() {
 
 struct Solution;
 
-try binary logic!
-impl Solution {
-
-    pub fn binary_gap(n: i32) -> i32 {
-        let _binary_value = format!("{:b}", n);
-        let mut res =0;
-        let mut pre_index:usize=0;
-        for (index,value) in _binary_value.chars().enumerate(){
-            if value=='1'{
-                let diff = index- pre_index;
-                if res<diff{
-                    res = diff
-                }
-                pre_index = index;
-            }
-        }
-        return res as i32;
-    }
-}
-
-
-
-
 // Runtime: 0 ms, faster than 100.00% of Rust online submissions for Binary Gap.
 // Memory Usage: 2 MB, less than 33.33% of Rust online submissions for Binary Gap.
 /*
